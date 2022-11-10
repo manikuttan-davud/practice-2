4a673357d1659c707b236a205fc0841e4a9267b8
Davud Sagar <davudsagar23@gmail.com>
2022-11-10 23:25:07 +0530

    new files added

---
+++ lib/screen/homeScreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class screebHome extends StatefulWidget {
+  const screebHome({super.key});
+
+  @override
+  State<screebHome> createState() => _screebHomeState();
+}
+
+class _screebHomeState extends State<screebHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: Colors.blue)
+        ),
+      ),
+    );
+  }
+}
